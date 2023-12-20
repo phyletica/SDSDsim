3c26bdc83ba4e9762a4e0773a60a7f30978de016
Jamie Oaks <joaks@adaptivebiotech.com>
2023-12-20 13:32:50 -0600

    Fixing test file names.

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -2,48 +2,24 @@
 
 import os
 import sys
-import math
-import random
 import pytest
 
-from sdsdsim import rng_utils
+from sdsdsim import math_utils
 
 
-class TestGetWeightedIndex:
-    def test_uniform(self):
-        rng = random.Random(1)
-        weights = [3, 3, 3, 3]
-        counts = [0 for i in range(len(weights))]
-        n = 1000000
-        for rep in n:
-            idx = rng_utils.get_weighted_index(weights, rng)
-            counts[idx] += 1
-        props = [c / n for c in counts]
-        for p in props:
-            assert math.isclose(p, 1.0 / len(weights))
+class TestIsZero:
+    def test_true(self):
+        assert math_utils.is_zero(1.0, 1.0)
+        assert math_utils.is_zero(-1.0, 1.0)
+        assert math_utils.is_zero(1e-11, 1e-11)
+        assert math_utils.is_zero(-1e-11, 1e-11)
+        assert math_utils.is_zero(1e-11, 1e-10)
+        assert math_utils.is_zero(-1e-11, 1e-10)
 
-    def test_nonuniform(self):
-        rng = random.Random(1)
-        weights = [5, 1, 3, 1]
-        expected = [ w / sum(weights) for w in weights ]
-        counts = [0 for i in range(len(weights))]
-        n = 1000000
-        for rep in n:
-            idx = rng_utils.get_weighted_index(weights, rng)
-            counts[idx] += 1
-        props = [c / n for c in counts]
-        for i in range(len(weights)):
-            assert math.isclose(props[i], expected[i]) 
-
-    def test_zero(self):
-        rng = random.Random(1)
-        weights = [5, 0, 3, 2]
-        expected = [ w / sum(weights) for w in weights ]
-        counts = [0 for i in range(len(weights))]
-        n = 1000000
-        for rep in n:
-            idx = rng_utils.get_weighted_index(weights, rng)
-            counts[idx] += 1
-        props = [c / n for c in counts]
-        for i in range(len(weights)):
-            assert math.isclose(props[i], expected[i]) 
+    def test_false(self):
+        assert not math_utils.is_zero(1.000000000000001, 1.0)
+        assert not math_utils.is_zero(-1.000000000000001, 1.0)
+        assert not math_utils.is_zero(1.1e-11, 1e-11)
+        assert not math_utils.is_zero(-1.1e-11, 1e-11)
+        assert not math_utils.is_zero(1e-9, 1e-10)
+        assert not math_utils.is_zero(-1e-9, 1e-10)

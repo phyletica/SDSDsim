124728094062601740b101887d31bb307aa7ffd4
Jamie R. Oaks <joaks1@gmail.com>
2024-01-09 21:02:14 -0600

    Getting CLI tool up and running.

--- src/sdsdsim/__init__.py
+++ src/sdsdsim/__init__.py
@@ -9,3 +9,9 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 GLOBAL_RNG = random.Random()
 
+import sdsdsim.argparse_utils
+import sdsdsim.rng_utils
+import sdsdsim.ctmc
+import sdsdsim.math_utils
+import sdsdsim.model
+import sdsdsim.node
--- src/sdsdsim/cli/sim_SDSD_trees.py
+++ src/sdsdsim/cli/sim_SDSD_trees.py
@@ -6,9 +6,7 @@ import random
 import argparse
 import yaml
 
-from sdsdsim import model
-from sdsdsim import argparse_utils
-from sdsdsim import __version__
+import sdsdsim
 
 
 def vet_model_config(model_config):
@@ -24,11 +22,11 @@ def vet_model_config(model_config):
     ]
     for k in model_config.keys():
         if k not in required_model_keys:
-            sys.stderr.write(f"ERROR: Unexpected model field: '{k}'")
+            sys.stderr.write(f"ERROR: Unexpected model field: '{k}'\n")
             sys.exit(1)
     for k in required_model_keys:
         if k not in model_config:
-            sys.stderr.write(f"ERROR: Model field '{k}' is missing")
+            sys.stderr.write(f"ERROR: Model field '{k}' is missing\n")
             sys.exit(1)
 
 def parse_stopping_conditions(d):
@@ -41,19 +39,19 @@ def parse_stopping_conditions(d):
     all_null = True
     for key, val in d.items():
         if key not in defaults:
-            sys.stderr.write(f"ERROR: Unexpected stopping condition: '{key}'")
+            sys.stderr.write(f"ERROR: Unexpected stopping condition: '{key}'\n")
             sys.exit(1)
         if val is not None:
             all_null = False
             if val <= 0:
                 sys.stderr.write(
                     f"ERROR: Stopping condition {key} should be positive or "
-                    f"null (found {val})"
+                    f"null (found {val})\n"
                 )
                 sys.exit(1)
         defaults[key] = val
     if all_null:
-        sys.stderr.write("ERROR: No stopping conditions provided")
+        sys.stderr.write("ERROR: No stopping conditions provided\n")
         sys.exit(1)
     return defaults
 
@@ -63,12 +61,12 @@ def parse_settings(settings_config):
         'keep_extinct_trees', False)
     settings['prune_extinct_leaves'] = settings_config.get(
         'prune_extinct_leaves', False)
-    stopping_conditions = settings.get('stopping_conditions', {})
+    stopping_conditions = settings_config.get('stopping_conditions', {})
     settings['stopping_conditions'] = parse_stopping_conditions(
         stopping_conditions)
     for k in settings_config.keys():
         if k not in settings:
-            sys.stderr.write(f"ERROR: Unrecognized settings field '{k}'")
+            sys.stderr.write(f"ERROR: Unrecognized settings field '{k}'\n")
     return settings
 
 def parse_config(path):
@@ -84,31 +82,31 @@ def main():
     parser.add_argument(
         'config_path',
         metavar = 'PATH-TO-CONFIG-FILE',
-        type = argparse_utils.arg_is_file,
+        type = sdsdsim.argparse_utils.arg_is_file,
         help = ('Path to SDSD model config file.'),
     )
     parser.add_argument(
         '-n', '--number-of-samples',
         action = 'store',
         default = 10,
-        type = argparse_utils.arg_is_positive_int,
+        type = sdsdsim.argparse_utils.arg_is_positive_int,
         help = ('Number of trees to sample.'),
     )
     parser.add_argument(
         '-s', '--seed',
         action = 'store',
-        type = argparse_utils.arg_is_positive_int,
+        type = sdsdsim.argparse_utils.arg_is_positive_int,
         help = ('Seed for random number generator.'),
     )
     args = parser.parse_args()
 
     rng = random.Random()
     if not args.seed:
-        args.seed = nephila.rng_utils.get_safe_seed(rng)
+        args.seed = sdsdsim.rng_utils.get_safe_seed(rng)
     rng.seed(args.seed)
 
     data = {
-        'version' : __version__,
+        'SDSDsim_version' : sdsdsim.__version__,
         'seed' : args.seed,
     }
 
@@ -120,12 +118,12 @@ def main():
     data['model'] = cfg['model']
     data['settings'] = cfg['settings']
 
-    model = model.SDSDModel(**cfg['model'])
+    model = sdsdsim.model.SDSDModel(**cfg['model'])
 
     samples = []
     
     while len(samples) < args.number_of_samples:
-        survived, tree, burst_times = model.sim_SDSD_tree(
+        survived, tree, burst_times = sdsdsim.model.sim_SDSD_tree(
             rng_seed = rng.random(),
             sdsd_model = model,
             **cfg['settings']['stopping_conditions']
@@ -137,9 +135,9 @@ def main():
         samples.append(
             {
                 'tree': tree.as_newick_string(),
-                'burst_times': burst_times,
+                'burst_times': [float(t) for t in burst_times],
             }
         )
 
     data['trees'] = samples
-    yaml.dump(data, stream = sys.stdout)
+    yaml.dump(data, stream = sys.stdout, default_flow_style = False)
